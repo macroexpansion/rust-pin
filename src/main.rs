35d01125a723b5779f12453d838e278baf1c213b
blueyellowpink <andrew@trasua.dev>
2023-10-05 01:37:31 +0700

    slow read with box

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use std::{
 };
 
 use futures::FutureExt;
-use tokio::time::{sleep, Sleep};
+use tokio::{
+    fs::File,
+    io::{AsyncRead, AsyncReadExt, ReadBuf},
+    time::{sleep, Sleep},
+};
 
 struct SleepFuture {
     sleep: Pin<Box<Sleep>>,
@@ -36,10 +40,45 @@ impl Future for SleepFuture {
     }
 }
 
+struct SlowRead<R> {
+    reader: Pin<Box<R>>,
+}
+
+impl<R> SlowRead<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader: Box::pin(reader),
+        }
+    }
+}
+
+impl<R> AsyncRead for SlowRead<R>
+where
+    R: AsyncRead,
+{
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        self.reader.as_mut().poll_read(cx, buf)
+    }
+}
+
+async fn slow_read() -> Result<(), Box<tokio::io::Error>> {
+    let mut buf = vec![0u8; 128 * 1024];
+    let reader = File::open("/dev/urandom").await?;
+    let mut slow_reader = SlowRead::new(reader);
+    slow_reader.read_exact(&mut buf).await?;
+    Ok(())
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let future = SleepFuture::new();
 
     let t = tokio::spawn(future);
     let _ = t.await;
+
+    slow_read().await.unwrap();
 }
